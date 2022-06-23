6af2d9a200b5adb826be5a1c6a90b4948ac8852f
TadeDerhbi <104700241+TadeDerhbi@users.noreply.github.com>
2022-06-23 18:26:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let firstname = "Oreoluwa "
+let middlename = "Deborah "
+let lastname = "Oguntade "
+let height= "5 feet six inches tall"
+let country= "Nigeria"
+alert("I am " + firstname + middlename + lastname + ".")
+alert( "I am " + height + ".")
+alert( "I am "+ "from " + country + ".")
